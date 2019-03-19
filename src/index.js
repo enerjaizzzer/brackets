cb40f1e2a7d9f0d3f98381f82a9c5456272aac46
enerjaizzzer <enerjaizzzer@gmail.com>
2019-03-19 09:05:16 +0300

    complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  for (var char of str) {
+    var sign = {};
+
+    for (var i = 0, l = bracketsConfig.length; i < l; i++) {
+      if (char === bracketsConfig[i][0]) {
+        sign.value = char;
+        sign.opened = true;
+        sign.closer = bracketsConfig[i][1];
+
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+          sign.special = i;
+        }
+
+      } else if (char === bracketsConfig[i][1]) {
+        sign.value = char;
+        sign.opened = false;
+      }
+    }
+
+    if (arr.length !== 0) {
+      if (sign.special === arr[arr.length - 1].special && sign.special !== undefined) {
+        arr.pop();
+        continue;
+      }
+    }
+
+    if (sign.opened) {
+      arr.push(sign);
+    } else if (arr.length === 0) {
+      return false;
+    } else if (sign.value === arr[arr.length - 1].closer) {
+      arr.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return arr.length === 0;
 }
